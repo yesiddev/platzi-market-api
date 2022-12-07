4f9bc1f1ba69753ffcf2ca19231251c8041518ed
yesiddev <yesiddeveloper@gmail.com>
2022-12-07 18:55:50 -0500

    add product controller methods

---
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -0,0 +1,53 @@
+package com.platzi.market.web.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.platzi.market.domain.dto.ProductDTO;
+import com.platzi.market.domain.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+  @Autowired
+  private ProductService productService;
+
+  @GetMapping("/all")
+  public List<ProductDTO> getAll() {
+    return productService.getAll();
+  }
+
+  @GetMapping("/category/{id}")
+  public Optional<List<ProductDTO>> getByCategory(int categoryId) {
+    return productService.getByCategory(categoryId);
+  }
+
+  @GetMapping("/scarse/{quantity}")
+  public Optional<List<ProductDTO>> getScarseProducts(int quantity) {
+    return productService.getScarseProducts(quantity);
+  }
+
+  @GetMapping("/{id}")
+  public Optional<ProductDTO> getProduct(int productId) {
+    return productService.getProduct(productId);
+  }
+
+  @PostMapping("/save")
+  public ProductDTO save(ProductDTO product) {
+    return productService.save(product);
+  }
+
+  @DeleteMapping("/delete/{id}")
+  public boolean delete(int productId) {
+    return productService.delete(productId);
+  }
+
+}
