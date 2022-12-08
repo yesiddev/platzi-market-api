df3cb3aa00e035d7a5470fc0646e571b4580da1a
yesiddev <yesiddeveloper@gmail.com>
2022-12-07 19:31:26 -0500

    add @RequestBody

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,7 +43,7 @@ public class ProductController {
   }
 
   @PostMapping("/save")
-  public ProductDTO save(ProductDTO product) {
+  public ProductDTO save(@RequestBody ProductDTO product) {
     return productService.save(product);
   }
 
